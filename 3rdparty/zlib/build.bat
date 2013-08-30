1c65417717343869eafc9a42f6247f56e980b225
wyrover <wyrover@gmail.com>
2013-08-30 14:15:48 +0800

    * 使用cmake生成zlib工程

--- 3rdparty/zlib/build.bat
+++
@@ -1,6 +0,0 @@
-echo "zlib-1.2.8"
-cd %~dp0
-nmake -f win32/Makefile.msc zlib.lib
-copy "zlib.lib" "../../lib/win32d/zlib_d.lib"
-copy "zlib.lib" "../../lib/win32/zlib.lib"
-nmake -f win32\Makefile.msc clean
